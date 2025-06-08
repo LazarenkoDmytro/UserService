90044a46d1325a90863f783020d4832ac16e959b
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-06-08 16:52:22 +0300

    init

---
+++ src/main/java/ua/org/kse/blablacar/BlaBlaCarApplication.java
@@ -0,0 +1,13 @@
+package ua.org.kse.blablacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaBlaCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlaBlaCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/org/kse/blablacar/BlaBlaCarApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.org.kse.blablacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaBlaCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
