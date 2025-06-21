9960a25a0aba6535d7e9bf6b5b73aaf341210f5a
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2025-06-21 12:46:20 +0300

    Init

---
+++ src/main/java/ua/org/blablacar/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.org.blablacar.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
